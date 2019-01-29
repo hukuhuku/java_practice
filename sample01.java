a6b200991f43997860c3610e4666d9e0de42d41b
hukuhuku <hukuhuku11111arm52@gmail.com>
2019-01-29 13:28:55 +0900

    [add]HelloWorld

---
+++ sample01.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class sample01 {
+	/**
+	* @param args
+	*/
+	public static void main(String[] args) {
+	// TODO 自動生成されたメソッド・スタブ
+	System.out.println("Hello World!");
+	}
+
+}
