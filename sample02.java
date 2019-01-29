fa01f3c077371888565802c9bd6a2d21b658d568
hukuhuku <hukuhuku11111arm52@gmail.com>
2019-01-29 13:52:14 +0900

    [update]変数代入

--- sample02.java
+++ sample02.java
@@ -2,9 +2,13 @@ package practice;
 
 public class sample02 {
 
-
 	public static void main(String[] args) {
 	// TODO 自動生成されたメソッド・スタブ
 		System.out.println(127);
+		System.out.println("a");
+
+		int number;
+		number = 10;
+		System.out.println(number);
 	}
 }
