00b3b672f3d171a698951a6a4843ddd76b9f2e1c
hukuhuku <hukuhuku11111arm52@gmail.com>
2019-02-01 19:58:07 +0900

    [add]window表示

---
+++ Wind.java
@@ -0,0 +1,12 @@
+package practice;
+
+import javax.swing.JFrame;
+
+public class Wind {
+	public static void main(String args[]) {
+		JFrame frame = new JFrame("ウィンドウを表示");
+		frame.setSize(850,700);
+		frame.setVisible(true);
+	}
+
+}
