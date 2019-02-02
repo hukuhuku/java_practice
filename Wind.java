bcb3fa1e6820aa5de48d6bbac4f6d9e1e006564a
hukuhuku <hukuhuku11111arm52@gmail.com>
2019-02-02 15:47:50 +0900

    [add]textbox追加

--- Wind.java
+++ Wind.java
@@ -1,12 +1,37 @@
 package practice;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
-public class Wind {
+class Wind extends JFrame{
 	public static void main(String args[]) {
-		JFrame frame = new JFrame("ウィンドウを表示");
-		frame.setSize(850,700);
+		Wind frame = new Wind("Title");
 		frame.setVisible(true);
-	}
 
+	}
+	Wind(String title){
+		setTitle(title);
+		setBounds(1000,1000,2000,1600);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    JPanel p = new JPanel();
+
+	    JTextArea area1 = new JTextArea();
+	    JTextArea area2 = new JTextArea(3, 20);
+	    area2.setPreferredSize(new Dimension(600, 500));
+	    JTextArea area3 = new JTextArea();
+	    area3.setRows(6);
+	    area3.setColumns(15);
+
+	    p.add(area1);
+	    p.add(area2);
+	    p.add(area3);
+
+	    Container contentPane = getContentPane();
+	    contentPane.add(p, BorderLayout.CENTER);
+	}
 }
